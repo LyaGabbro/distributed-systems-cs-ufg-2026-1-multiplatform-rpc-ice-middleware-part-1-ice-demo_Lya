175fddd3e7b856b1598a45a59339faa87b3c976b
Lya Ribeiro <157846604+LyaGabbro@users.noreply.github.com>
2026-04-13 15:35:06 -0300

    made a small comment on client.py

--- client.py
+++ client.py
@@ -3,9 +3,10 @@ import Demo
  
 communicator = Ice.initialize(sys.argv)
 
+#inserir ip da máquina na qual o código vai rodar no lugar de 98.90.53.6 na linha abaixo
 base = communicator.stringToProxy("SimplePrinter:tcp -h 98.90.53.6 -p 11000")
 printer = Demo.PrinterPrx.checkedCast(base)
 if not printer:
     raise RuntimeError("Invalid proxy")
 
-printer.printString("Hello World!")
+printer.printString("Hello! This is a string from the printer.")
